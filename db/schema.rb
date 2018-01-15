763d795e9de0d11702513dfb51900d696e2c30f8
eugene.bbr <eugene.bbr@gmail.com>
2018-01-15 16:36:29 +0300

    question counter added

--- db/schema.rb
+++ db/schema.rb
@@ -13,24 +13,23 @@
 ActiveRecord::Schema.define(version: 20171124131314) do
 
   create_table "questions", force: :cascade do |t|
-    t.string   "text"
-    t.string   "answer"
+    t.string "text"
+    t.string "answer"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "user_id"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
-  add_index "questions", ["user_id"], name: "index_questions_on_user_id"
-
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "username"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.string   "email"
-    t.string   "password_hash"
-    t.string   "password_salt"
-    t.string   "avatar_url"
+    t.string "name"
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email"
+    t.string "password_hash"
+    t.string "password_salt"
+    t.string "avatar_url"
   end
 
 end
